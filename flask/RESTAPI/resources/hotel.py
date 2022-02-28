65f9d5cb7a03cba2671689c90093d60df9e4171c
Micael Cosmo <micael.trabalho@hotmail.com>
2022-02-28 12:00:44 -0300

    Update hotel.py Criado nova classe que comporta as funções CRUD.

--- flask/RESTAPI/resources/hotel.py
+++ flask/RESTAPI/resources/hotel.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource
 
-
 hoteis = [
     {
         'hotel_id': 'alpha',
@@ -30,3 +29,19 @@ class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404  # not found
+
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
