3f24327deca08a3fd28908af634b5985d1e82677
Micael Cosmo <micael.trabalho@hotmail.com>
2022-02-28 10:12:34 -0300

    Update app.py Estrutura para iniciar uma API utilizando FLASK.

---
+++ flask/RESTAPI/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': 'meus hoteis'}
+
+api.add_resource(Hoteis, '/hoteis')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# http://127.0.0.1:5000/hoteis
