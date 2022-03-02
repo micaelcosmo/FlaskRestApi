aa75737c40c575ea05f84aadb703c33ac9d3cc27
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-02 11:45:28 -0300

    Update app.py, hotel.py, and sql_alchemy.py Utilizado SQLAlchemy para criar banco de dados automaticamente

--- flask/RESTAPI/app.py
+++ flask/RESTAPI/app.py
@@ -3,12 +3,20 @@ from flask_restful import Resource, Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
 
 # http://127.0.0.1:5000/hoteis
