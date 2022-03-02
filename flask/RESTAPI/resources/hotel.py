e687d2c5e375e9df1437f3180abf8a7fbae06326
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-02 10:10:55 -0300

    Update hotel.py Função delete agora remove um hotel da lista de hoteis. Listandos os hoteis diferentes do hotel desejado.

--- flask/RESTAPI/resources/hotel.py
+++ flask/RESTAPI/resources/hotel.py
@@ -65,6 +65,7 @@ class Hotel(Resource):
         hoteis.append(novo_hotel)
         return novo_hotel, 201  # created
 
-
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted.'}
