50b3a8950b2d4c6714286db4d7cec5e86cdb359e
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-03 07:48:38 -0300

    Update hotel.py Atualização de POST hotel, fazendo interação  com o novo modelo. Agora post retorna mensagem caso já exista no banco de dados.

--- flask/RESTAPI/resources/hotel.py
+++ flask/RESTAPI/resources/hotel.py
@@ -21,17 +21,19 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404  # not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400  # Bad Request
+
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_objeto = HotelModel(hotel_id, **dados)
         novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
             return novo_hotel, 200
