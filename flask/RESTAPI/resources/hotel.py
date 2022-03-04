e27afdd877ba205fc49da8aa64bc7e07bc11cd6c
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-04 09:54:17 -0300

    Update hotel.py Atualização do GET hotel. Refatorado caminho para chamada do método que procura hotel, retornando com um dicionário.

--- flask/RESTAPI/resources/hotel.py
+++ flask/RESTAPI/resources/hotel.py
@@ -15,9 +15,9 @@ class Hotel(Resource):
     argumentos.add_argument('cidade')
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found.'}, 404  # not found
 
     def post(self, hotel_id):
