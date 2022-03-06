7be4dc1670fffa55ed2a62c6f021139117d72d54
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-06 20:56:15 -0300

    Novo recurso para criar cadastros. Recebe um login e senha, e o salva no banco de dados caso não exista ainda

--- flask/RESTAPI/resources/usuario.py
+++ flask/RESTAPI/resources/usuario.py
@@ -3,7 +3,7 @@ from models.usuario import UserModel
 
 
 class User(Resource):
-
+    #  /usuario/{user_id}
     def get(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -19,3 +19,19 @@ class User(Resource):
                 return {'message': 'An error occurred trying to delete user.'}, 500
             return {'message': 'User deleted.'}
         return {'message': 'User not Found.'}, 404
+
+
+class UserRegister(Resource):
+    #  /cadastro/
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "The login '{}' already exists.".format(dados['login'])}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User created successfully'}, 201
