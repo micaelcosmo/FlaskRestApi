5b65837fa11d95c67cbe17f1d93aa841bd1e1edb
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-06 21:00:26 -0300

    Adicionado recurso para cadastro de usuário.

--- flask/RESTAPI/app.py
+++ flask/RESTAPI/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User
+from resources.usuario import User, UserRegister
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -17,10 +17,11 @@ def cria_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User, '/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
     app.run(debug=True)
 
-# http://127.0.0.1:5000/hoteis
+# http://127.0.0.1:5000/
