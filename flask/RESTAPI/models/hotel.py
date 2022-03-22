3f3dc3b2cb36b81b9c4dbec0a9fcbb8b13483e5c
Micael Cosmo <micael.trabalho@hotmail.com>
2022-03-22 09:18:29 -0300

    Integração de modelo de site.

--- flask/RESTAPI/models/hotel.py
+++ flask/RESTAPI/models/hotel.py
@@ -9,6 +9,8 @@ class HotelModel(banco.Model):
     estrelas = banco.Column(banco.Float(precision=1))
     diaria = banco.Column(banco.Float(precision=2))
     cidade = banco.Column(banco.String(40))
+    site_id = banco.Column(banco.Integer, banco.ForeignKey('sites.site_id'))
+    #site = banco.relationship('SiteModel')
 
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
@@ -16,6 +18,7 @@ class HotelModel(banco.Model):
         self.estrelas = estrelas
         self.diaria = diaria
         self.cidade = cidade
+        self.site_id = site_id
 
     def json(self):
         return {
@@ -24,6 +27,7 @@ class HotelModel(banco.Model):
             'estrelas': self.estrelas,
             'diaria': self.diaria,
             'cidade': self.cidade
+            'site_id': self.site_id
         }
 
     @classmethod
